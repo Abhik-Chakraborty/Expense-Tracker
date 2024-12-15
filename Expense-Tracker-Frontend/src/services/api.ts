7d6661d5f20b949a14be58a1e83d247420904978
Abhik-Chakraborty <abhik17.cfc@gmail.com>
2024-12-15 22:41:44 +0530

    evn setup modification

--- Expense-Tracker-Frontend/src/services/api.ts
+++ Expense-Tracker-Frontend/src/services/api.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import { Expense } from '../types/expense';
 
-const API_URL = 'http://localhost:8081/api/expenses';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8081/api/expenses';
 
 export const getExpenses = () => axios.get<Expense[]>(API_URL);
 export const createExpense = (expense: Expense) => axios.post<Expense>(API_URL, expense);
 export const deleteExpense = (id: number) => axios.delete(`${API_URL}/${id}`);
+
+
+
+
